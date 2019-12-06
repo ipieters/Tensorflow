df61568d72c7bb83f789923df08d84facb3cc87a
ipieters <ipiet870@mtroyal.ca>
2019-12-06 09:50:49 -0700

    Added part 3

--- Part 1/MNISTStarter.py
+++ Part 1/MNISTStarter.py
@@ -1,9 +1,7 @@
-#For Google Collab
-#try:
-#  # %tensorflow_version only exists in Colab.
-#  %tensorflow_version 2.x
-#except Exception:
-#  pass
+# Igor Pieters
+# UCID: 30061116
+# CPSC 501 - Fall 2019
+# Assignment 1 - Part 1
 
 import tensorflow as tf
 
@@ -11,22 +9,29 @@ print("--Get data--")
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-print("--Process data--")
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
 print("--Make model--")
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(10, activation='sigmoid')
+  tf.keras.layers.Dense(1024, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
-model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-print("--Fit model--")
-model.fit(x_train, y_train, epochs=1, verbose=2)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-print("--Evaluate model--")
-model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
+print("--Fit train model--")
+model.fit(x_train, y_train, epochs=10, verbose=2)
+
+
+print("--Evaluate train model--")
+model_loss, model_acc = model.evaluate(x_train,  y_train, verbose=2)
 print(f"Model Loss:    {model_loss:.2f}")
 print(f"Model Accuray: {model_acc*100:.1f}%")
 
+print("--Evaluate test model--")
+model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
+print(f"Model Loss:    {model_loss:.2f}")
+print(f"Model Accuray: {model_acc*100:.1f}%")
 #Save Model
+model.save('MNIST.h5')
